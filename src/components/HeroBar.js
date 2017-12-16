88b388f6386e9660434e5fa4bfbe3b2b63371ca0
ek1506 <anonymousemail@gmail.com>
2017-12-16 06:27:43 -0800

    Add HeroBar component

---
+++ src/components/HeroBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroBar = () => (
+  <section className="hero is-primary">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Divinity Original Sin 2 Character Builder</h1>
+      </div>
+    </div>
+  </section>
+);
+
+export default HeroBar;
--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import HeroBar from './HeroBar';
 
 const RootComponent = () => (
   <div>
     <Navbar />
+    <HeroBar />
     <div>
       <p>This is the Root component!</p>
       <button className="button">Button!</button>
