bc1288a025333629d369591e92eb0bfffec76b5f
ek1506 <anonymousemail@gmail.com>
2018-01-24 09:39:50 -0800

    Fix Hero Bar typo

--- src/components/HeroBar.js
+++ src/components/HeroBar.js
@@ -4,7 +4,7 @@ const HeroBar = () => (
   <section className="hero is-primary">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title">Divinity Original Sin 2 Character Builder</h1>
+        <h1 className="title">Divinity Original Sin 2 Character Planner</h1>
       </div>
     </div>
   </section>
